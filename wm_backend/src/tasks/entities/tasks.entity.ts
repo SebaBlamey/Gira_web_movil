938976cb5880358f1f82ddc2456191bcb19abbec
felipe guerra <felipe.guerra01@alumnos.ucn.cl>
2023-12-01 00:47:59 -0300

    anexo

--- wm_backend/src/tasks/entities/tasks.entity.ts
+++ wm_backend/src/tasks/entities/tasks.entity.ts
@@ -1,23 +1,30 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 import { Trabajo } from 'src/proyect/entities/trabajo.entity';
+import { User } from 'src/users/user.entity';
 
 @Schema()
 export class Task extends Document {
   @Prop({ required: true })
   nombre: string;
 
-  @Prop({ type: Types.ObjectId, ref: 'Trabajo', default: null, required: false })
-  proyectID?: string;
+  @Prop({ type: Types.ObjectId, ref: 'Trabajo', default: null })
+  proyectID: Trabajo;
 
-  @Prop({ type: [{ user: { type: Types.ObjectId, ref: 'User' }}] })
-  userID?: { user: Types.ObjectId };
+  @Prop({ type: Types.ObjectId, ref: 'User' })
+  userID?: User;
 
   @Prop()
   observacion?: string;
 
+  @Prop({ required: false }) 
+  fechaInicio?: string;
+
+  @Prop({ required: false }) 
+  fechaFin?: string;
+
   @Prop({ required: true })
-  estado: 'PENDIENTE' | 'EN PROCESO' | 'COMPLETADO';
+  estado: 'PENDIENTE' | 'EN PROCESO' | 'COMPLETADO' | 'CERRADO';
 
   @Prop({ type: [{ usuario: { type: Types.ObjectId, ref: 'User' }, comentario: String }] })
   comentarios?: { usuario: Types.ObjectId, comentario: string }[];
--- wm_backend/src/tasks/tasks.module.ts
+++ wm_backend/src/tasks/tasks.module.ts
@@ -4,11 +4,19 @@ import { TaskController } from './tasks.controller';
 import { TaskService } from './tasks.service';
 import { Task, TaskSchema } from './entities/tasks.entity';
 import { TrabajoModule } from '../proyect/trabajo.module';
+import { Trabajo, TrabajoSchema } from 'src/proyect/entities/trabajo.entity';
+import { UserModule } from 'src/users/user.module';
+import { User, UserSchema } from 'src/users/user.entity';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema },
+                              {name: Trabajo.name, schema: TrabajoSchema},
+                            {name: User.name, schema: UserSchema}
+
+    ]),
     TrabajoModule,
+    UserModule,
   ],
   controllers: [TaskController],
   providers: [TaskService],
